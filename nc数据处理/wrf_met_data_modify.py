2e3d11d6f6b20d9dcfd4f4bc73ce6916b772f010
steven <z786909151@163.com>
2020-09-28 21:31:47 +0800

    modify wrf_met_data_modify.py

--- nc数据处理/wrf_met_data_modify.py
+++ nc数据处理/wrf_met_data_modify.py
@@ -12,18 +12,23 @@ dir_met = [home + dir_ for dir_ in dir_met]
 
 
 
-for dir_alone in dir_met:
-    rootgrp = Dataset(dir_alone, "a")
-    rootgrp.NUM_METGRID_SOIL_LEVELS = 4
-    rootgrp.close()
+# for dir_alone in dir_met:
+#     rootgrp = Dataset(dir_alone, "a")
+#     rootgrp.NUM_METGRID_SOIL_LEVELS = 4
+#     rootgrp.close()
+#
+# # 概览
+# rootgrp = Dataset(dir_met[0], "r")
+# print(rootgrp.variables.keys())
+# print('****************************')
+# print(rootgrp)
+# print('****************************')
+# print(rootgrp.ncattrs())
+# print('****************************')
+# print(rootgrp.NUM_METGRID_SOIL_LEVELS)
+# rootgrp.close()
 
-# 概览
-rootgrp = Dataset(dir_met[0], "r")
-print(rootgrp.variables.keys())
-print('****************************')
-print(rootgrp)
-print('****************************')
-print(rootgrp.ncattrs())
-print('****************************')
-print(rootgrp.NUM_METGRID_SOIL_LEVELS)
-rootgrp.close()
+# rename,win下不能执行，因为路径名不能包含:，需要去linux下执行
+# for dir_alone in dir_met:
+#     new_dir = dir_alone.replace("^%", ":")
+#     os.rename(dir_alone, new_dir)
