75c9ab2e13ddc259b0c079d8323cdb521eaaa158
steven <z786909151@163.com>
2020-11-04 20:55:34 +0800

    modify extract_nc_daily.py

--- flash dorught/extract_nc_sm_daily.py
+++ flash dorught/extract_nc_sm_daily.py
@@ -1,3 +1,9 @@
 # code: utf-8
 # author: "Xudong Zheng" 
 # email: Z786909151@163.com
+import extract_nc_daily as ex
+
+path = "D:\GLADS\daily_data"
+coord_path = "H:\GIS\Flash_drought\coord.txt"
+ex.overview(path)
+# ex.extract_nc_daily(path, coord_path, )
--- nc数据处理/extract_nc_daily.py
+++ nc数据处理/extract_nc_daily.py
@@ -1,8 +1,7 @@
 # code: utf-8
 # author: "Xudong Zheng" 
 # email: Z786909151@163.com
-# extract variable(given region by coord) from .nc file
-# 处理nc文件，将GLDASmonthly 0.25 at GES DISC 1948-2014读取lp范围的 Rainf_f_tavg 数据到数组中
+# extract variable(given region by coord) from .nc4 file
 import numpy as np
 from netCDF4 import Dataset
 import os
@@ -24,7 +23,7 @@ def extract_nc_daily(path, coord_path, variable_name, precision=3):
     """
     coord = pd.read_csv(coord_path, sep=",")  # read coord(extract by fishnet)
     coord = coord.round(precision)  # 处理单位以便与nc中lat lon一致
-    result = path + "/" + os.listdir(path)
+    result = [path + "/" + d for d in os.listdir(path) if d[-4:] == ".nc4"]
     variable = np.zeros((len(result), len(coord)))
 
     # calculate the index of lat/lon in coord from source nc file
@@ -58,8 +57,10 @@ def extract_nc_daily(path, coord_path, variable_name, precision=3):
 
 def overview(path):
     # overview of the nc file
-    result = path + "/" + os.listdir(path)
+    result = [path + "/" + d for d in os.listdir(path) if d[-4:] == ".nc4"]
     rootgrp = Dataset(result[0], "r")
+    print('****************************')
+    print(f"number of nc file:{len(result)}")
     print('****************************')
     print(f"key:{rootgrp.variables.keys()}")
     print('****************************')
@@ -68,6 +69,11 @@ def overview(path):
     print(f"lat:{rootgrp.variables['lat'][:]}")
     print('****************************')
     print(f"lon:{rootgrp.variables['lon'][:]}")
+    variable_name = input("variable name:")  # if you want to see the variable, input its name here
+    while variable_name != "":
+        print('****************************')
+        print(f"variable:{rootgrp.variables[variable_name]}")
+        variable_name = input("variable name:")  # if you want to quit, input enter here
     rootgrp.close()
 
 
