b8ddaeca3786981ead1f13438936acb58c07d8cd
steven <z786909151@163.com>
2020-11-10 16:55:10 +0800

    add calss FD_RI(FD)

--- flash dorught/FDIP.py
+++ flash dorught/FDIP.py
@@ -77,7 +77,7 @@ class Drought(SM_percentile):
 
         output:
             self.SM_percentile: SM_percentile calculated by SM using self.cal_SM_percentile
-            self.dry_flag_start, self.dry_flag_end: start end of each drought events
+            self.dry_flag_start, self.dry_flag_end: start end of each drought events(shrinkable)
             self.DD, self.DS, self.SM_min: character of each drought events
             plot: use self.plot(plot(self, title="Drought", yes=0)), save figure set yes = 1: plot dorught
             xlsx: use self.out_put(self, xlsx=1) , set xlsx=1: out put drought the xlsx
@@ -96,13 +96,14 @@ class Drought(SM_percentile):
         SM_percentile.__init__(self, SM, timestep)
         self.dry_flag_start, self.dry_flag_end = self.run_threshold(self.SM_percentile, self.threshold1)
         self.eliminate()
-        self.DD, self.DS, self.SM_min = self.character()
+        self.DD, self.DS, self.SM_min, self.SM_min_flag = self.character()
 
     @staticmethod
     def run_threshold(index: np.ndarray, threshold: float) -> (np.ndarray, np.ndarray):
         """ run_threshold to identify dry bell (start-end)
         index: the base index
         threshold: the threshold to identify dry bell(index < threshold)
+        point explain(discrete): start < threshold, end < threshold --> it is shrinkable and strict
         """
         # define drought based on index and threshold
         dry_flag = np.argwhere(index <= threshold).flatten()
@@ -136,7 +137,9 @@ class Drought(SM_percentile):
         DS = np.array([x[self.dry_flag_start[i]: self.dry_flag_end[i] + 1].sum() for i in range(n)], dtype='float')
         SM_min = np.array([min(self.SM_percentile[self.dry_flag_start[i]: self.dry_flag_end[i] + 1]) for i in range(n)]
                           , dtype='float')
-        return DD, DS, SM_min
+        SM_min_flag = np.array([np.argmin(self.SM_percentile[self.dry_flag_start[i]: self.dry_flag_end[i] + 1]) +
+                                self.dry_flag_start[i] for i in range(n)], dtype='float')
+        return DD, DS, SM_min, SM_min_flag
 
     def out_put(self, xlsx=0) -> pd.DataFrame:
         """ output the drought event to a dataframe and .xlsx file(to set xlsx=1) """
@@ -147,9 +150,9 @@ class Drought(SM_percentile):
         threshold2 = np.full((n,), self.threshold2, dtype='float')
         Drought_character = pd.DataFrame(
             np.vstack((Date_start, Date_end, self.dry_flag_start, self.dry_flag_end, self.DD, self.DS, self.SM_min,
-                       threshold1, threshold2)).T,
-            columns=(
-                "Date_start", "Date_end", "flag_start", "flag_end", "DD", "DS", "SM_min", "thrshold1", "threshold2"))
+                       self.SM_min_flag, threshold1, threshold2)).T,
+            columns=("Date_start", "Date_end", "flag_start", "flag_end", "DD", "DS", "SM_min", "SM_min_flag"
+                     , "thrshold1", "threshold2"))
         if xlsx == 1:
             Drought_character.to_excel("/Drought_character", index=False)
         return Drought_character
@@ -200,6 +203,12 @@ class FD(Drought):
     def __init__(self, SM, timestep=365, Date=0, threshold1=0.4, threshold2=0.2, RI_threshold=0.1,
                  RI_mean_threshold=0.065):
         """
+        Identify flash drought based on rule: RI > threshold -> fd_flag_start, fd_flag_end, eliminate no flash develop
+        events by RImean < RI_mean_threshold
+
+        this identification regard flash drought as a flash develop period of a normal drought event, and a normal
+        drought can contain more than one flash drought
+
         input:
             SM: SOIL MOISTURE, list or numpy array
             threshold1: the threshold to identify dry bell
@@ -215,26 +224,27 @@ class FD(Drought):
                 x ：x data in one year
 
         output:
-            self.SM_percentile:
+            self.SM_percentile: SM_percentile calculated by SM using self.cal_SM_percentile
             self.dry_flag_start, self.dry_flag_end: start end of each drought events
             self.DD, self.DS, self.SM_min: character of each drought events
-            fd_flag_start, fd_flag_end, RImean, RImax: character of each drought events's flash develop periods
-            plot: use self.plot(plot(self, title="Drought", yes=0)), save figure set yes = 1: plot dorught
+            fd_flag_start, fd_flag_end, RImean, RImax: character of each drought events's flash develop periods, shrinkable start point
+            plot: use self.plot(plot(self, title="Drought", yes=0)), save figure set yes = 1: plot dorught, plot flash drought using compute start point[-1]
             xlsx: use self.out_put(self, xlsx=1) , set xlsx=1: out put drought the xlsx
 
         """
-        Drought.__init__(self, SM, timestep=365, Date=0, threshold1=0.4, threshold2=0.2)
+        Drought.__init__(self, SM, timestep, Date, threshold1, threshold2)
         self.RI_threshold = RI_threshold
         self.RI_mean_threshold = RI_mean_threshold
-        self.fd_flag_start, self.fd_flag_end, self.RImean, self.RImax = self.develop_period()
+        self.fd_flag_start, self.fd_flag_end, self.RImean, self.RImax, self.RI = self.develop_period()
         self.fd_eliminate()
         self.dp, self.FDD, self.FDS = self.fd_character()
 
     @staticmethod
     def fd_run_threshold(index, threshold):
-        """ run_threshold to identify develop period
+        """ run_threshold to identify develop period (index > threshold, different with run_threshold)
         index: the base index
         threshold: the threshold to identify develop period(index > threshold)
+        point explain(discrete): start > threshold, end > threshold --> it is shrinkable and strict
         """
         # define develop period based on index and threshold
         dry_flag = np.argwhere(index >= threshold).flatten()
@@ -253,15 +263,22 @@ class FD(Drought):
     def develop_period(self) -> (list, list, list, list):
         """ extract extract flash development period of drought event, RI is instantaneous
         unit = 1/time interval of input SM
+
         return fd_flag_start, fd_flag_end, RImean, RImax for each flash drought event of every drought events
+        list[array, array, ...] --> list: n(drought) array: m(flash)
         SM[--drought1[--flash develop period1[--fd_flag_start, fd_flag_end, RImean, RImax--],..flash develop periodn[]--], ...droughtn[]--]
         """
         n = len(self.dry_flag_start)  # the number of drought events
+        #  distinguish the compute start point and flag start point:
+        # compute start point: compute from -1, it means the develop period from no-drought to drought
+        # flag start point: it means the point has been achieved the threshold condition (match the shrinkable
+        # run threshold and RI(SM(t)-SM(t-1))(same length with RI))
         RI = self.SM_percentile - np.append(self.SM_percentile[0], self.SM_percentile[:-1])
         RI = RI * -1
         # calculate RI of the whole timeseries of SM_peritile, then extract drought/flash develop period from it,
         # hypothesize RI in the first position is zero(self.SM_percentile[0]-self.SM_percentile[0])
         # diff= sm[t]-sm[t-1] < 0: develop: RI > 0 ——> multiply factor: -1
+        # list[array, array, ...] --> list: n(drought) array: m(flash)
         fd_flag_start, fd_flag_end, RImean, RImax = [], [], [], []
         # list, each element is a df_flag_start/end/RImean/RImax series of a drought event
         # namely, it represents the develop periods(number > 1) of a dought event
@@ -281,7 +298,7 @@ class FD(Drought):
             fd_flag_end.append(fd_flag_end_)  # np.ndarray
             RImean.append(RImean_)  # np.ndarray
             RImax.append(RImax_)
-        return fd_flag_start, fd_flag_end, RImean, RImax
+        return fd_flag_start, fd_flag_end, RImean, RImax, RI
 
     def fd_eliminate(self):
         """ eliminate develop period which are not flash based on RI_mean_threshold of each drought event
@@ -295,11 +312,12 @@ class FD(Drought):
                 if self.RImean[i][j] < self.RI_mean_threshold:
                     indexi.append(i)
                     indexj.append(j)
-        for k in range(len(indexi)):
-            self.fd_flag_start[indexi[k]] = np.delete(self.fd_flag_start[indexi[k]], indexj[k])
-            self.fd_flag_end[indexi[k]] = np.delete(self.fd_flag_end[indexi[k]], indexj[k])
-            self.RImean[indexi[k]] = np.delete(self.RImean[indexi[k]], indexj[k])
-            self.RImax[indexi[k]] = np.delete(self.RImax[indexi[k]], indexj[k])
+        if len(indexi) > 0:
+            for k in range(len(indexi)):
+                self.fd_flag_start[indexi[k]] = np.delete(self.fd_flag_start[indexi[k]], indexj[k])
+                self.fd_flag_end[indexi[k]] = np.delete(self.fd_flag_end[indexi[k]], indexj[k])
+                self.RImean[indexi[k]] = np.delete(self.RImean[indexi[k]], indexj[k])
+                self.RImax[indexi[k]] = np.delete(self.RImax[indexi[k]], indexj[k])
         # TODO add rule to elominate drought event with RI_mean > RImean_threshold but duration too short
         #  合并连续骤旱（合并发展阶段），剔除小骤旱
 
@@ -338,10 +356,9 @@ class FD(Drought):
         RI_mean_threshold = np.full((n,), self.RI_mean_threshold, dtype='float')
         Drought_character = pd.DataFrame(
             np.vstack((Date_start, Date_end, self.dry_flag_start, self.dry_flag_end, self.DD, self.DS, self.SM_min,
-                       threshold1, threshold2, RI_threshold, RI_mean_threshold)).T,
-            columns=(
-                "Date_start", "Date_end", "flag_start", "flag_end", "DD", "DS", "SM_min", "thrshold1", "threshold2",
-                "RI_threshold", "RI_mean_threshold"))
+                       self.SM_min_flag, threshold1, threshold2, RI_threshold, RI_mean_threshold)).T,
+            columns=("Date_start", "Date_end", "flag_start", "flag_end", "DD", "DS", "SM_min", "SM_min_flag",
+                     "thrshold1", "threshold2", "RI_threshold", "RI_mean_threshold"))
         Drought_character["fd_flag_start"] = self.fd_flag_start
         Drought_character["fd_flag_end"] = self.fd_flag_end
         Drought_character["RImean"] = self.RImean
@@ -382,14 +399,17 @@ class FD(Drought):
                 self.SM_percentile[self.dry_flag_start[i]:self.dry_flag_end[i] + 1]
         ax2.fill_between(self.Date, events, self.threshold1, alpha=0.5, facecolor="red",
                          label="Drought events", interpolate=True)
-        # plot flash drought
+        # plot flash drought (from compute start point: means change from no-drought)
         for i in range(len(self.fd_flag_start)):
             for j in range(len(self.fd_flag_start[i])):
-                start = self.fd_flag_start[i][j]
+                if self.fd_flag_start[i][j] == 0:  # check if it is the first point
+                    start = self.fd_flag_start[i][j]
+                else:
+                    start = self.fd_flag_start[i][j] - 1
                 end = self.fd_flag_end[i][j]
                 ax2.plot(self.Date[start:end + 1], self.SM_percentile[start:end + 1], color="r", linewidth=2, marker=7,
                          markersize=6)
-                print(i, j)
+                print(f"drought: {i}", ";", f"flash develop period: {j}")
         # set figure
         ax1.set_ylabel("SM")
         ax2.set_ylabel("SM Percentile")
@@ -404,9 +424,100 @@ class FD(Drought):
             plt.savefig("Drought/" + title)
 
 
+class FD_RI(FD):
+    def __init__(self, SM, timestep=365, Date=0, threshold1=0.4, threshold2=0.2, RI_threshold=0.1,
+                 RI_mean_threshold=0.065):
+        """
+        Identify flash drought based on rule: RI > threshold -> fd_flag_end
+        But fd_flag_start = dry_flag_start and there is no eliminate procedure for not flash event
+        (this identification class inherit FD and modify develop_period function and fd_eliminate function)
+
+        each dorught have no more than one flash drought development
+
+        input:
+            SM: SOIL MOISTURE, list or numpy array
+            threshold1: the threshold to identify dry bell
+            threshold2: the threshold to eliminate mild drought events
+            RI_threshold: the threshold of RI to eliminate mild flash drought events which not flash(RI max)
+            RI_mean_threshold: the RI_mean_threshold to eliminate mild flash drought events which not flash(RI mean)
+            Date: the Date of SM
+            timestep: the timestep of SM （the number of SM data in one year）, which is used to do cal_SM_percentile
+            (reshape a vector to a array, which shape is (n(year)+1 * timestep))
+                365 : daily, 365 data in one year
+                12 : monthly, 12 data in one year
+                73 : pentad(5), 73 data in one year
+                x ：x data in one year
+
+        output:
+            self.SM_percentile: SM_percentile calculated by SM using self.cal_SM_percentile
+            self.dry_flag_start, self.dry_flag_end: start end of each drought events
+            self.DD, self.DS, self.SM_min: character of each drought events
+            fd_flag_start, fd_flag_end, RImean, RImax: character of each drought events's flash develop periods, shrinkable start point
+            plot: use self.plot(plot(self, title="Drought", yes=0)), save figure set yes = 1: plot dorught, plot flash drought using compute start point[-1]
+            xlsx: use self.out_put(self, xlsx=1) , set xlsx=1: out put drought the xlsx
+
+        """
+        FD.__init__(self, SM, timestep, Date, threshold1, threshold2, RI_threshold, RI_mean_threshold)
+
+    def develop_period(self) -> (list, list, list, list):
+        """ extract extract flash development period of drought event, RI is instantaneous
+        unit = 1/time interval of input SM
+        return fd_flag_start, fd_flag_end, RImean (list: keep the same output with FD to use inherited function,
+        such as plot and ouput)
+        """
+        n = len(self.dry_flag_start)  # the number of drought events
+        RI = self.SM_percentile - np.append(self.SM_percentile[0], self.SM_percentile[:-1])
+        RI = RI * -1
+        # calculate RI of the whole timeseries of SM_peritile, then extract drought/flash develop period from it,
+        # hypothesize RI in the first position is zero(self.SM_percentile[0]-self.SM_percentile[0])
+        # diff= sm[t]-sm[t-1] < 0: develop: RI > 0 ——> multiply factor: -1
+
+        # extract drought/flash develop period from each dorught using RI
+        # list[array, array, ...] --> list: n(drought) array: m(flash: 0 or 1)
+        fd_flag_start = [np.array([self.dry_flag_start[i]]) for i in range(n)]
+        fd_flag_end, RImean, RImax = [], [], []
+        for i in range(n):
+            start = self.dry_flag_start[i]
+            end = self.dry_flag_end[i]
+            #  distinguish the compute start point and flag start point:
+            # compute start point: compute from -1, it means the develop period from no-drought to drought
+            # flag start point: it means the point has been achieved the threshold condition (match the shrinkable
+            # run threshold)
+            # compute RI_mean from start - 1(compute start point, RI_[0] = SM[0] - SM[-1]) -> flag start point
+            RI_ = RI[start: end + 1]
+            RImean_ = np.array([RI_[:i + 1].mean() for i in range(end - start + 1)])
+            fd_flag_end_ = np.argwhere(RI_ <= self.RI_threshold)
+            # check the fd_flag_end_ to see if it is empty
+            if len(fd_flag_end_) != 0:
+                if fd_flag_end_.flatten()[0] != 0:
+                    fd_flag_end_ = fd_flag_end_.flatten()[0]  # index of end based on RI_
+                    fd_flag_end_ = fd_flag_end_ - 1  # shrinkable(point <= self.RI_threshold -> point - 1): must be (end
+                    # > threshold) -> flag end
+                    fd_flag_end.append(np.array([fd_flag_end_ + start]))  # index of end based on SM index/RI,
+                    RImean.append(np.array([RImean_[fd_flag_end_]]))
+                    RImax.append(np.array([RI_[: fd_flag_end_ + 1].max()]))
+                else:
+                    fd_flag_end.append(np.array([]))  # There is no flash drought
+                    fd_flag_start[i] = np.array([])
+                    RImean.append(np.array([]))
+                    RImax.append(np.array([]))
+            else:
+                fd_flag_end.append(np.array([]))  # There is no flash drought
+                fd_flag_start[i] = np.array([])
+                RImean.append(np.array([]))
+                RImax.append(np.array([]))
+        return fd_flag_start, fd_flag_end, RImean, RImax, RI
+
+    def fd_eliminate(self):
+        """ There is no eliminate procedure for not flash event """
+        pass
+
+
 if __name__ == "__main__":
-    sm = np.random.rand(365, )
-    FD1 = FD(sm, 365)
+    sm = np.random.rand(365*5, )
+    sm = np.convolve(sm, np.repeat(1/3, 3), mode='full')  # running means
+    FD1 = FD_RI(sm, 365)
+    RI = FD1.RI
     FD1.plot()
     print(FD1.out_put())
     out = FD1.out_put()
