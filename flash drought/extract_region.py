c45ab39f85039aa2dde9310196a158d1942c658b
steven <z786909151@163.com>
2021-04-01 15:14:15 +0800

    add pd.dataframe extract method

--- flash drought/extract_region.py
+++ flash drought/extract_region.py
@@ -44,55 +44,78 @@ class index_coord:
 class extract_region(index_coord):
     ''' class to extract region data from original data, inherit from index_coord '''
 
-    def extract_data(self, coord_region: pd.DataFrame, coord_original: pd.DataFrame, data_original: np.ndarray,
-                     axis: int = 0):
+    def extract_data(self, coord_region: pd.DataFrame, coord_original: pd.DataFrame, data_original, axis: int = 0):
         ''' extract region data from original data
         input:
             coord_region: pd.DataFrame, region coord, it should have the same format with coord_orginal and be part of
                           coord_original
             coord_orginal: pd.DataFrame, original coord, it should contain columns "lat" & "lon", every in this dataframe
                           is unique value
-            data_original: 1D or 2D np.ndarray
+            data_original: 1D or 2D np.ndarray / pd.Dataframe
             axis: int, 0 or 1, the axis corresponding coord(grid), such as data_original(n*m), n: time, m: coord(grid)
                  -> axis=1, default = 0
         out:
             ret: result, which has the same dimension with data_original, but the "coord" dimension has been extracted
-                (len  = len(coord_region))
+                (len  = len(coord_region)), if data_original is np.ndarray / pd.Dataframe, it will be np.ndarray /
+                pd.Dataframe
+
+        note:
+            1) index_col=0: pd.read_excel(os.path.join(home, "5.Analysis_spatial/static_params.xlsx"), index_col=0)
         '''
         index = self.cal_index(coord_region, coord_original)
+        self.df_true = isinstance(data_original, pd.DataFrame)
+
+        if self.df_true:
+            columns = data_original.columns
+            data_original = data_original.to_numpy()
+
         if len(data_original.shape) == 1:
             ret = data_original[index]
-            return ret
         elif len(data_original.shape) == 2:
             if axis == 0:
                 ret = data_original[index, :]
-                return ret
             elif axis == 1:
                 data_ = data_original.T
                 ret = data_[index, :].T
-                return ret
             else:
                 raise ValueError("axis: int, 0 or 1, but others is given")
         else:
             raise IndexError("too many indices for data_original array: array is 1/2-dimensional, but more were given")
+
+        if self.df_true:
+            ret = pd.DataFrame(ret, columns=columns)
+
+        self.ret = ret
+
+        return ret
+
+    def save(self, path=None):
+        ''' save extract data
+        input:
+            path: save path, if dara_original is pd.Dataframe, path should end with '.xlsx', if it is np.ndarray, it
+                  should end with '.txt'
+        '''
+        if self.df_true:
+            if path == None:
+                path = 'extract.xlsx'
+            self.ret.to_excel(path)
+        else:
+            if path == None:
+                path = 'extract.txt'
+            np.savetxt(path, self.ret)
 
 
 def extract_sm_static_params():
     # original data
     sm_rz = np.loadtxt(data_path, dtype="float", delimiter=" ")
     static_params = pd.read_excel(os.path.join(home, "5.Analysis_spatial/static_params.xlsx"), index_col=0)
-    static_params_array = static_params.to_numpy()
 
     # extract data
     extract_region_ = extract_region()
     sm_rz_Helong = extract_region_.extract_data(coord_Helong, coord, sm_rz, axis=1)
     sm_rz_noHelong = extract_region_.extract_data(coord_noHelong, coord, sm_rz, axis=1)
-    static_params_Helong = extract_region_.extract_data(coord_Helong, coord, static_params_array, axis=0)
-    static_params_noHelong = extract_region_.extract_data(coord_noHelong, coord, static_params_array, axis=0)
-
-    # pd.DataFrame
-    static_params_Helong = pd.DataFrame(static_params_Helong, columns=static_params.columns)
-    static_params_noHelong = pd.DataFrame(static_params_noHelong, columns=static_params.columns)
+    static_params_Helong = extract_region_.extract_data(coord_Helong, coord, static_params, axis=0)
+    static_params_noHelong = extract_region_.extract_data(coord_noHelong, coord, static_params, axis=0)
 
     # save data
     np.savetxt(os.path.join(home, "sm_rz_Helong.txt"), sm_rz_Helong)
@@ -106,21 +129,12 @@ def extract_Drought_FD_number():
     Drought_year_number = pd.read_excel(os.path.join(home, "Drought_year_number.xlsx"), index_col=0)
     FD_year_number = pd.read_excel(os.path.join(home, "FD_year_number.xlsx"), index_col=0)
 
-    Drought_year_number_array = Drought_year_number.to_numpy()
-    FD_year_number_array = FD_year_number.to_numpy()
-
     # extract data
     extract_region_ = extract_region()
-    Drought_year_number_Helong = extract_region_.extract_data(coord_Helong, coord, Drought_year_number_array, axis=0)
-    Drought_year_number_noHelong = extract_region_.extract_data(coord_noHelong, coord, Drought_year_number_array, axis=0)
-    FD_year_number_Helong = extract_region_.extract_data(coord_Helong, coord, FD_year_number_array, axis=0)
-    FD_year_number_noHelong = extract_region_.extract_data(coord_noHelong, coord, FD_year_number_array, axis=0)
-
-    # pd.DataFrame
-    Drought_year_number_Helong = pd.DataFrame(Drought_year_number_Helong, columns=Drought_year_number.columns)
-    Drought_year_number_noHelong = pd.DataFrame(Drought_year_number_noHelong, columns=Drought_year_number.columns)
-    FD_year_number_Helong = pd.DataFrame(FD_year_number_Helong, columns=FD_year_number.columns)
-    FD_year_number_noHelong = pd.DataFrame(FD_year_number_noHelong, columns=FD_year_number.columns)
+    Drought_year_number_Helong = extract_region_.extract_data(coord_Helong, coord, Drought_year_number, axis=0)
+    Drought_year_number_noHelong = extract_region_.extract_data(coord_noHelong, coord, Drought_year_number, axis=0)
+    FD_year_number_Helong = extract_region_.extract_data(coord_Helong, coord, FD_year_number, axis=0)
+    FD_year_number_noHelong = extract_region_.extract_data(coord_noHelong, coord, FD_year_number, axis=0)
 
     # save data
     Drought_year_number_Helong.to_excel(os.path.join(home, "Drought_year_number_Helong.xlsx"))
@@ -151,10 +165,4 @@ if __name__ == "__main__":
     # extract_sm_static_params()
 
     # extract_Drought_FD_number
-    extract_Drought_FD_number()
-
-
-
-
-
-
+    # extract_Drought_FD_number()
