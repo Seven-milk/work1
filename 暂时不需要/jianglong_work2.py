8f579f3f02ed6e91c276fbee7bde9670e0cd74f0
Seven-milk <786909151@qq.com>
2021-04-12 22:37:52 +0800

    modify jianglong_work2.py

--- 暂时不需要/jianglong_work2.py
+++ 暂时不需要/jianglong_work2.py
@@ -5,13 +5,21 @@ import pandas as pd
 import numpy as np
 import os
 
-home = "F:/work/jianglong/data"
+home = "F:/work/jianglong/jianglong_work2"
 stand_path = "stander.xlsx"
 data_path = "2001-2005_max.csv"
 
 stand = pd.read_excel(os.path.join(home, stand_path))
 data = np.loadtxt(os.path.join(home, data_path), dtype=float)
-filename = [f"{year}_{month}_max.csv" for year in range(2001, 2006) for month in range(1, 13)]
+
+filename = []
+for year in range(2001, 2006):
+    for month in range(1, 13):
+        if month < 10:
+            filename.append(f"{year}_0{month}_max.csv")
+        else:
+            filename.append(f"{year}_{month}_max.csv")
+
 filename = [os.path.join(home, filename_) for filename_ in filename]
 
 for i in range(data.shape[1]):
@@ -19,4 +27,3 @@ for i in range(data.shape[1]):
     stand["Value"] = data_
     stand["Value"] = stand["Value"].astype(int)
     stand.to_csv(filename[i], index=False)
-
