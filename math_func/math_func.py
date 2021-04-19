c6e5b9fca1000fb8f71340e37fc3fe74a9a6c9c7
Seven-milk <786909151@qq.com>
2021-04-19 10:29:58 +0800

    add intersection

--- math_func/math_func.py
+++ math_func/math_func.py
@@ -28,9 +28,47 @@ def sortWithIndex(data, p=False, **kwargs):
 
     return sortedindex, sorteddata
 
+def intersection(line1, line2):
+    ''' calculate intersection point from two lines
+    x21, y21             x12, y12
+
+               (x, y)
+
+    x11, y11             x22, y22
+
+    (y22 - y21) * x + (x21 - x22) * y = x21 * y22 - x22 * y21
+    (y12 - y11) * x + (x11 - x12) * y = x11 * y12 - x12 * y11
+
+    AX = b
+     |(y22 - y21)   (x21 - x22)|  x  |  =  | x21 * y22 - x22 * y21 |
+    |(y12 - y11)   (x11 - x12)|  y  |  =  | x11 * y12 - x12 * y11 |
+
+    input:
+        line1/2: list, [x11, y11, x12, y12], [x21, y21, x22, y22], that two lines have four points will be calculated
+                intersection
+
+    output:
+        r: np.array[x, y], x = r[0], y = r[1]
+
+    '''
+    # unpack
+    x11, y11, x12, y12 = line1
+    x21, y21, x22, y22 = line2
+
+    A = [[y22 - y21, x21 - x22],
+        [y12 - y11, x11 - x12]]
+
+    b = [x21 * y22 - x22 * y21, x11 * y12 - x12 * y11]
+
+    r = np.linalg.solve(A, b)
+
+    return r
+
+
 
 if __name__ == '__main__':
     data = np.arange(10)
     slope_ = slope(data)
     data = [1, 3, 2, 0]
     sortedindex, sorteddata = sortWithIndex(data, p=True)
+    r = intersection([1, 2, 2, 1], [1, 1, 2, 2])
