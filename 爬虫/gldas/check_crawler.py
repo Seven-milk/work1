d2731b9866ef83df9a457090faf7b47c60d2259f
Seven-milk <786909151@qq.com>
2021-04-23 11:54:39 +0800

    complete check_crawler.py

--- 爬虫/gldas/check_crawler.py
+++ 爬虫/gldas/check_crawler.py
@@ -18,7 +18,6 @@ with open(URL, 'r') as file:
 # file_name_all
 urls = urls[1:]
 file_name_all = [re.search(r"LABEL.*\d\.nc4", url)[0][6:] for url in urls]
-file_name_all = [os.path.join(home, file) for file in file_name_all]
 
 # file_name_downloaded
 file_name_downloaded = [path for path in os.listdir(home) if path[-4:] == ".nc4"]
@@ -29,5 +28,23 @@ file_name_downloaded = set(file_name_downloaded)
 
 # difference set
 file_name_notdownloaded = file_name_all - file_name_downloaded
+file_name_notdownloaded = list(file_name_notdownloaded)
+# time_notdownload = [filename_[18: -8] for filename_ in list(file_name_notdownloaded)]
 
-# refactor url
+# refactor url, this way is not robust
+# re.findall(r'\d{8}\.\d{4}', url_template)
+# url_template = urls[0]
+# re.sub(r'\d{8}\.\d{4}', time_notdownload[0], url_template)
+
+# search url_notdownload
+file_name_all = [re.search(r"LABEL.*\d\.nc4", url)[0][6:] for url in urls]  # make sure the rank is right
+url_notdownload = []
+for file_name_nd in file_name_notdownloaded:
+    index_ = [file_name_nd in name for name in file_name_all].index(True)
+    url_notdownload.append(urls[index_])
+
+url_notdownload = "\n".join(url_notdownload)
+
+# save
+with open(os.path.join(home, 'url_notdownload.txt'), 'w', encoding='utf-8') as f:
+    f.write(url_notdownload)
