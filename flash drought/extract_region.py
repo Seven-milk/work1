94ed293d24b161c2a013deee8fcf8d1f74d008b8
Seven-milk <786909151@qq.com>
2021-05-04 16:23:37 +0800

    change into Workflow

--- flash drought/extract_region.py
+++ flash drought/extract_region.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import numpy as np
 import warnings
+import Workflow
 
 warnings.filterwarnings("ignore")
 
@@ -105,23 +106,78 @@ class extract_region(index_coord):
             np.savetxt(path, self.ret)
 
 
+class ExtractRegion(Workflow.WorkBase):
+    ''' Work, extract region data from all data, i.e. sm_rz to sm_rz_Helong '''
+
+    def __init__(self, coord_region: pd.DataFrame, coord_original: pd.DataFrame, data_original, axis: int = 0,
+                 save_path=None, info=""):
+        ''' init function
+        input:
+            similar with extract_region
+            save_path: str, home path to save, if save_path=None(default), do not save
+            info: str, informatiom for this Class to print and save in save_path, shouldn't too long
+        '''
+        self.coord_region = coord_region
+        self.coord_original = coord_original
+        self.data_original = data_original
+        self.axis = axis
+        self.save_path = save_path
+        self._info = info
+
+    def run(self):
+        ''' implement WorkBase.run '''
+        extract_region_ = extract_region()
+        region_data = extract_region_.extract_data(self.coord_region, self.coord_original, self.data_original,
+                                                   self.axis)
+
+        # save result
+        if self.save_path != None:
+            if extract_region_.df_true:
+                suffix = ".xlsx"
+            else:
+                suffix = ".txt"
+            extract_region_.save(os.path.join(self.save_path, f"region_data_{self._info}{suffix}"))
+
+        return region_data
+
+    def __repr__(self):
+        return f"This is ExtractRegion, info: {self._info}, extract region data from all data"
+
+    def __str__(self):
+        return f"This is ExtractRegion, info: {self._info}, extract region data from all data"
+
+
 def extract_sm_static_params():
     # original data
     sm_rz = np.loadtxt(data_path, dtype="float", delimiter=" ")
     static_params = pd.read_excel(os.path.join(home, "5.Analysis_spatial/static_params.xlsx"), index_col=0)
 
-    # extract data
-    extract_region_ = extract_region()
-    sm_rz_Helong = extract_region_.extract_data(coord_Helong, coord, sm_rz, axis=1)
-    sm_rz_noHelong = extract_region_.extract_data(coord_noHelong, coord, sm_rz, axis=1)
-    static_params_Helong = extract_region_.extract_data(coord_Helong, coord, static_params, axis=0)
-    static_params_noHelong = extract_region_.extract_data(coord_noHelong, coord, static_params, axis=0)
+    # extract data based on ExtractRegion(WorkFlow)
+    ER_sm_rz_Helong = ExtractRegion(coord_Helong, coord, sm_rz, axis=1, save_path=None, info="sm_rz_Helong")  # home
+    ER_sm_rz_noHelong = ExtractRegion(coord_noHelong, coord, sm_rz, axis=1, save_path=None, info="sm_rz_noHelong")
+    ER_static_params_Helong = ExtractRegion(coord_Helong, coord, static_params, axis=0, save_path=None,
+                                            info="static_params_Helong")
+    ER_static_params_noHelong = ExtractRegion(coord_noHelong, coord, static_params, axis=0, save_path=None,
+                                              info="static_params_noHelong")
+
+    # WF add
+    WF_sm_static_params = Workflow.WorkFlow(ER_sm_rz_Helong, ER_sm_rz_noHelong, ER_static_params_Helong,
+                                            ER_static_params_noHelong)
+    WF_sm_static_params.runflow()
+    return WF_sm_static_params
+
+    # extract data based on extract_region
+    # extract_region_ = extract_region()
+    # sm_rz_Helong = extract_region_.extract_data(coord_Helong, coord, sm_rz, axis=1)
+    # sm_rz_noHelong = extract_region_.extract_data(coord_noHelong, coord, sm_rz, axis=1)
+    # static_params_Helong = extract_region_.extract_data(coord_Helong, coord, static_params, axis=0)
+    # static_params_noHelong = extract_region_.extract_data(coord_noHelong, coord, static_params, axis=0)
 
     # save data
-    np.savetxt(os.path.join(home, "sm_rz_Helong.txt"), sm_rz_Helong)
-    np.savetxt(os.path.join(home, "sm_rz_noHelong.txt"), sm_rz_noHelong)
-    static_params_Helong.to_excel(os.path.join(home, "static_params_Helong.xlsx"))
-    static_params_noHelong.to_excel(os.path.join(home, "static_params_noHelong.xlsx"))
+    # np.savetxt(os.path.join(home, "sm_rz_Helong.txt"), sm_rz_Helong)
+    # np.savetxt(os.path.join(home, "sm_rz_noHelong.txt"), sm_rz_noHelong)
+    # static_params_Helong.to_excel(os.path.join(home, "static_params_Helong.xlsx"))
+    # static_params_noHelong.to_excel(os.path.join(home, "static_params_noHelong.xlsx"))
 
 
 def extract_Drought_FD_number():
@@ -129,18 +185,34 @@ def extract_Drought_FD_number():
     Drought_year_number = pd.read_excel(os.path.join(home, "Drought_year_number.xlsx"), index_col=0)
     FD_year_number = pd.read_excel(os.path.join(home, "FD_year_number.xlsx"), index_col=0)
 
-    # extract data
-    extract_region_ = extract_region()
-    Drought_year_number_Helong = extract_region_.extract_data(coord_Helong, coord, Drought_year_number, axis=0)
-    Drought_year_number_noHelong = extract_region_.extract_data(coord_noHelong, coord, Drought_year_number, axis=0)
-    FD_year_number_Helong = extract_region_.extract_data(coord_Helong, coord, FD_year_number, axis=0)
-    FD_year_number_noHelong = extract_region_.extract_data(coord_noHelong, coord, FD_year_number, axis=0)
+    # extract data based on ExtractRegion(WorkFlow)
+    ER_Drought_year_number_Helong = ExtractRegion(coord_Helong, coord, Drought_year_number, axis=0, save_path=None,
+                                                  info="Drought_year_number_Helong")  # home
+    ER_Drought_year_number_noHelong = ExtractRegion(coord_noHelong, coord, Drought_year_number, axis=0, save_path=None,
+                                                    info="Drought_year_number_noHelong")
+    ER_FD_year_number_Helong = ExtractRegion(coord_Helong, coord, FD_year_number, axis=0, save_path=None,
+                                             info="FD_year_number_Helong")
+    ER_FD_year_number_noHelong = ExtractRegion(coord_noHelong, coord, FD_year_number, axis=0, save_path=None,
+                                               info="FD_year_number_noHelong")
+
+    # WF add
+    WF_sm_static_params = Workflow.WorkFlow(ER_Drought_year_number_Helong, ER_Drought_year_number_noHelong,
+                                            ER_FD_year_number_Helong, ER_FD_year_number_noHelong)
+    WF_sm_static_params.runflow()
+    return WF_sm_static_params
+
+    # extract data based on extract_region
+    # extract_region_ = extract_region()
+    # Drought_year_number_Helong = extract_region_.extract_data(coord_Helong, coord, Drought_year_number, axis=0)
+    # Drought_year_number_noHelong = extract_region_.extract_data(coord_noHelong, coord, Drought_year_number, axis=0)
+    # FD_year_number_Helong = extract_region_.extract_data(coord_Helong, coord, FD_year_number, axis=0)
+    # FD_year_number_noHelong = extract_region_.extract_data(coord_noHelong, coord, FD_year_number, axis=0)
 
     # save data
-    Drought_year_number_Helong.to_excel(os.path.join(home, "Drought_year_number_Helong.xlsx"))
-    Drought_year_number_noHelong.to_excel(os.path.join(home, "Drought_year_number_noHelong.xlsx"))
-    FD_year_number_Helong.to_excel(os.path.join(home, "FD_year_number_Helong.xlsx"))
-    FD_year_number_noHelong.to_excel(os.path.join(home, "FD_year_number_noHelong.xlsx"))
+    # Drought_year_number_Helong.to_excel(os.path.join(home, "Drought_year_number_Helong.xlsx"))
+    # Drought_year_number_noHelong.to_excel(os.path.join(home, "Drought_year_number_noHelong.xlsx"))
+    # FD_year_number_Helong.to_excel(os.path.join(home, "FD_year_number_Helong.xlsx"))
+    # FD_year_number_noHelong.to_excel(os.path.join(home, "FD_year_number_noHelong.xlsx"))
 
 
 if __name__ == "__main__":
@@ -162,7 +234,7 @@ if __name__ == "__main__":
     index_noHelong = index_coord_.cal_index(coord_noHelong, coord)
 
     # extract_sm_static_params
-    # extract_sm_static_params()
+    wf_extract_sm_static_param = extract_sm_static_params()
 
     # extract_Drought_FD_number
-    # extract_Drought_FD_number()
+    wf_extract_Drought_FD_number = extract_Drought_FD_number()
