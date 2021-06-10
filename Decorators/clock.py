a0bc526e60155ec77ba1ef55af389b1d2825020d
Seven-milk <786909151@qq.com>
2021-06-10 23:27:41 +0800

    modify

--- Decorators/clock.py
+++ Decorators/clock.py
@@ -2,16 +2,24 @@
 # author: "Xudong Zheng" 
 # email: Z786909151@163.com
 import time
+import functools
 
 
 def clock(func):
     ''' decorator, print the time elapsed (and results) for func running '''
-    def clocked(*args):
-        t0 = time.perf_counter()
-        result = func(*args)
-        elapsed = time.perf_counter() - t0
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
         name = func.__name__
-        arg_str = ','.join(repr(arg) for arg in args)
+        arg_lst = []
+        if args:
+            arg_lst.append(','.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(','.join(pairs))
+        arg_str = ','.join(arg_lst)
         print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
         return result
     return clocked
